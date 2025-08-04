9aa484a6d52b83bc444b9ee1d7b5e532f5669a86
Dhruvil-Rangani <dhruvilrangani007@gmail.com>
2025-08-03 22:30:56 -0400

    Create wordLadder.java

---
+++ graphs/dfs-bfs problem/wordLadder.java
@@ -0,0 +1,41 @@
+class Solution {
+    static class Pair {
+        String word;
+        int steps;
+
+        Pair(String word, int steps) {
+            this.word = word;
+            this.steps = steps;
+        }
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(beginWord, 1));
+
+        Set<String> st = new HashSet<>(wordList);
+        st.remove(beginWord);
+
+        while(!q.isEmpty()) {
+            Pair pair = q.poll();
+            String word = pair.word;
+            int steps = pair.steps;
+
+            if(word.equals(endWord)) return steps;
+
+            for(int i = 0; i < word.length(); i++) {
+                char[] wordArray = word.toCharArray();
+                for(char c = 'a'; c <= 'z'; c++) {
+                    wordArray[i] = c;
+                    String newWord = new String(wordArray);
+                    if(st.contains(newWord)) {
+                        st.remove(newWord);
+                        q.add(new Pair(newWord, steps + 1));
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
