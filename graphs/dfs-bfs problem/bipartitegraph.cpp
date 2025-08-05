fdca9803a099cc69348d8e34cf3df1f5f17358ab
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-08-05 02:34:55 -0400

    Update bipartitegraph.cpp

--- graphs/dfs-bfs problem/bipartitegraph.cpp
+++ graphs/dfs-bfs problem/bipartitegraph.cpp
@@ -7,9 +7,9 @@ using namespace std;
 // This solution uses BFS to color the graph and check if it is bipartite.
 // The time complexity is O(V + E) where V is the number of vertices and E is the number of edges in the graph.
 
-class Solution{
+class SolutionBFS{
 private:
-    bool bfs(int start, int V, vector<int> adj[], int color[]) {
+    bool bfs(int start, int V, vector<int> adj[], vector<int> &color) {
         queue<int> q;
         q.push(start);
         color[start] = 0;
@@ -30,8 +30,7 @@ private:
     }
 public:
     bool isBipartite(int V, vector<int> adj[])  {
-        int color[V];
-        for(int i = 0; i < V; i++) color[i] = -1;
+        vector<int> color(V, -1);
 
         for(int i = 0; i < V; i++) {
             if(color[i] == -1) {
@@ -42,3 +41,32 @@ public:
         return true;
     }
 };
+
+// This is a C++ solution for the problem of checking if a graph is bipartite using DFS.
+class SolutionDFS{
+private:
+    bool dfs(int node, int col, vector<int> &color, vector<int> adj[]) {
+        color[node] = col;
+
+        for(auto adjNodes : adj[node]) {
+            if(color[adjNodes] == -1) {
+                if(!dfs(adjNodes, !col, color, adj)) return false;
+            } else if(color[adjNodes] == col) return false;
+        }
+
+        return true;
+    }
+public:
+    bool isBipartite(int V, vector<int> adj[])  {
+        vector<int> color(V, -1);
+
+        for(int i = 0; i < V; i++) {
+            if(color[i] == -1) {
+                if(!dfs(i, 0, color, adj)) return false;
+            }
+        }
+
+        return true;
+    }
+};
+
