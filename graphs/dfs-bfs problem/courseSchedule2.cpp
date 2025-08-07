efa518d5d737299751600b0ed7bfa5fb14351d6d
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-08-06 20:29:46 -0400

    Create courseSchedule2.cpp

---
+++ graphs/dfs-bfs problem/courseSchedule2.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <queue>
+using namespace std;
+// Course schedule 2
+// leetcode problem: https://leetcode.com/problems/course-schedule-ii/
+// Time Complexity: O(V + E)
+// Space Complexity: O(V + E)
+class Solution {
+  private:
+    vector<int> topo(int N, vector<int> adj[]) {
+      vector<int> inDegree(N, 0);
+      for(int i = 0; i < N; i++) {
+        for(auto it: adj[i]) inDegree[it]++;
+      }
+
+      vector<int> ans;
+      queue<int> q;
+
+      for(int i = 0; i < N; i++) {
+        if(inDegree[i] == 0) q.push(i);
+      }
+
+      while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        ans.push_back(node);
+
+        for(auto it: adj[node]) {
+          inDegree[it]--;
+          if(inDegree[it] == 0) q.push(it);
+        }
+      }
+
+      return ans;
+    }
+   public:
+    vector<int> findOrder(int N, vector<vector<int>> arr) {
+        vector<int> adj[N];
+        for (auto it : arr) {
+            int u = it[0];
+            int v = it[1];
+
+            adj[v].push_back(u);
+        }
+
+        vector<int> order = topo(N, adj);
+        if(N == order.size()) return order;
+        return {};
+    }
+};
