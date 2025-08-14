737d068e96f4107be704da40a69e2f6c6f65a046
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-08-13 22:58:17 -0400

    Update cityWithSmallestNeighbor.java

--- graphs/shortest path algo/cityWithSmallestNeighbor.java
+++ graphs/shortest path algo/cityWithSmallestNeighbor.java
@@ -53,4 +53,60 @@ class Solution {
 // Dijkstra's algorithm can be used to find the shortest paths from each city to all other cities.
 // This approach is more efficient than Floyd-Warshall for sparse graphs, as it runs in O((N + M) * log(N)) time complexity,
 // where N is the number of nodes and M is the number of edges.
+// below is the implementation of solving the problem using Dijkstra's algorithm:
 
+class Solution2 {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        List<int[]>[] adj = new ArrayList[n];
+        for(int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int[] edge : edges) {
+            adj[edge[0]].add(new int[]{edge[1], edge[2]});
+            adj[edge[1]].add(new int[]{edge[0], edge[2]});
+        }
+
+        int minCount = Integer.MAX_VALUE;
+        int ans = -1;
+
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+            int[] dist = new int[n];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[i] = 0;
+
+            PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+            pq.offer(new int[]{i, 0});
+
+            while(!pq.isEmpty()) {
+                int[] p = pq.poll();
+                int node = p[0];
+                int time = p[1];
+
+                if(time > distanceThreshold) continue;
+
+                for(int[] neighbor : adj[node]) {
+                    int nextNode = neighbor[0];
+                    int weight = neighbor[1];
+
+                    if(dist[nextNode] > time + weight) {
+                        dist[nextNode] = time + weight;
+                        pq.offer(new int[]{nextNode, dist[nextNode]});
+                    }
+                }
+            }
+
+            for(int j = 0; j < n; j++) {
+                if(i != j && dist[j] <= distanceThreshold) count++;
+            }
+
+            if(count < minCount || (count == minCount && i > ans)) {
+                minCount = count;
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+}
