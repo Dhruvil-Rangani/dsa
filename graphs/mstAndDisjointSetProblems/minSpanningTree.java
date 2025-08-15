06286be4ba8872030f959b91d898dfa84f9d583a
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-08-14 21:43:23 -0400

    Create minSpanningTree.java

---
+++ graphs/mstAndDisjointSetProblems/minSpanningTree.java
@@ -0,0 +1,8 @@
+// what is the minimum spanning tree of a graph?
+// The minimum spanning tree (MST) of a graph is a subset of the edges that connects all vertices together without any cycles and with the minimum possible total edge weight.
+// The MST is a tree that spans all the vertices in the graph, and it has the minimum sum of edge weights among all possible spanning trees.
+// The MST is useful in various applications, such as network design, clustering, and optimization problems.
+// The MST conditions are:
+// 1. The MST must include all vertices of the graph.
+// 2. The MST must not contain any cycles.
+// 3. The total weight of the edges in the MST must be minimized.
