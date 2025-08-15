0b57df95100041a8878284622b8ae3164fe852e8
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-08-14 21:44:25 -0400

    Update minSpanningTree.java

--- graphs/mstAndDisjointSetProblems/minSpanningTree.java
+++ graphs/mstAndDisjointSetProblems/minSpanningTree.java
@@ -6,3 +6,10 @@
 // 1. The MST must include all vertices of the graph.
 // 2. The MST must not contain any cycles.
 // 3. The total weight of the edges in the MST must be minimized.
+// The MST can be found using algorithms like Prim's or Kruskal's.
+// Use cases:
+// 1. Network design: To connect a set of points with the least cost.
+// 2. Clustering: To group data points based on their distances.
+// 3. Optimization problems: To minimize the cost of connecting various components in a system.
+// 4. Circuit design: To minimize the length of wires in electronic circuits.
+// 5. Transportation networks: To design efficient road or rail networks.
