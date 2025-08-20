8c0a9dbbe70437edd7669c5d8d9357b98699aaef
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-08-20 00:06:44 -0400

    Create climbingStairs.go

---
+++ dynamic programming/climbingStairs.go
@@ -0,0 +1,14 @@
+package climbStairs
+
+func climbStairs(n int) int {
+	prev2 := 1
+    prev1 := 1
+
+    for i := 2; i <= n; i++ {
+        curr := prev1 + prev2
+        prev2 = prev1
+        prev1 = curr
+    }
+
+    return prev1
+}
