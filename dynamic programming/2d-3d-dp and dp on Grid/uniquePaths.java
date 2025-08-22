7ed52efcd617806f8b74a9379b69b91ff1969b32
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-08-21 23:28:38 -0400

    Update uniquePaths.java

--- dynamic programming/2d-3d-dp and dp on Grid/uniquePaths.java
+++ dynamic programming/2d-3d-dp and dp on Grid/uniquePaths.java
@@ -40,3 +40,28 @@ class Solution1 {
     }
 }
 
+// Tabulation Approach
+// Time Complexity: O(m * n) where m is the number of rows and n is the number of columns
+// Space Complexity: O(m * n) for the dp table
+
+class Solution2 {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) dp[0][0] = 1;
+                else {
+                    int up = 0;
+                    int left = 0;
+
+                    if(i > 0) up = dp[i - 1][j];
+                    if(j > 0) left = dp[i][j - 1];
+                    dp[i][j] = up + left;
+                }
+
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
