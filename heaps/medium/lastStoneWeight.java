8646d5d34fd4815b4e158123f01a6408a07e3063
Dhruvil-Rangani <dhruvilrangani007@gmail.com>
2025-08-29 23:16:42 -0400

    Update lastStoneWeight.java

--- heaps/medium/lastStoneWeight.java
+++ heaps/medium/lastStoneWeight.java
@@ -28,3 +28,22 @@ class Solution {
         return 0;
     }
 }
+
+
+// well-written interview ready code
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int stone : stones) maxHeap.add(stone);
+
+        while(maxHeap.size() > 1) {
+
+            int x = maxHeap.poll();
+            int y = maxHeap.poll();
+            if(x != y) maxHeap.add(Math.abs(x - y));
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
+
