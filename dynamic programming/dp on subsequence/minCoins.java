b83555df67e1206d6b7014c56f615a95f961261e
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-04 19:13:16 -0400

    Update minCoins.java

--- dynamic programming/dp on subsequence/minCoins.java
+++ dynamic programming/dp on subsequence/minCoins.java
@@ -94,5 +94,52 @@ class Solution3 {
     }
 }
 
+// space optimization approach
+// Time Complexity: O(n * target) where n is the number of elements in arr and target is the target sum
+// Space Complexity: O(target)
+
+class Solution {
+    private int f(int i, int target, int[][] dp, int[] coins) {
+        if(i == 0) {
+            if(target % coins[i] == 0) return target/coins[i];
+            return (int)(1e9);
+        }
+
+        if(dp[i][target] != -1) return dp[i][target];
+
+        int notTake = 0 + f(i - 1, target, dp, coins);
+        int take = (int)(1e9);
+        if(coins[i] <= target) take = 1 + f(i, target - coins[i], dp, coins);
+
+        return dp[i][target] = Math.min(notTake, take);
+    }
+
+    public int MinimumCoins(int[] coins, int amount) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                dp[i] = i / coins[0];
+            else
+                dp[i] = (int)1e9;
+        }
+        
+
+        for(int i = 1; i < n; i++) {
+            int[] temp = new int[amount + 1];
+            for(int j = 0; j <= amount; j++) {
+                int notTake = 0 + dp[j];
+                int take = (int)(1e9);
+                if(coins[i] <= j) take = 1 + temp[j - coins[i]];
+                temp[j] = Math.min(notTake, take);
+            }
+            dp = temp;
+        }
+
+        return dp[amount] == (int)(1e9) ? -1 : dp[amount];
+    }
+}
+
+
 
 
