3c30754e9160f8aaa37eca5c1a8b6c2a0eb9264d
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-04 19:27:05 -0400

    Update minCoins.java

--- dynamic programming/dp on subsequence/minCoins.java
+++ dynamic programming/dp on subsequence/minCoins.java
@@ -99,32 +99,26 @@ class Solution3 {
 // Space Complexity: O(target)
 
 class Solution {
-    public int MinimumCoins(int[] coins, int amount) {
+    public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[] dp = new int[amount + 1];
-        for (int i = 0; i <= amount; i++) {
-            if (i % coins[0] == 0)
-                dp[i] = i / coins[0];
-            else
-                dp[i] = (int)1e9;
+        for(int i = 0; i <= amount; i++) {
+            if(i % coins[0] == 0) dp[i] = i / coins[0];
+            else dp[i] = (int)(1e9);
         }
-        
 
         for(int i = 1; i < n; i++) {
-            int[] temp = new int[amount + 1];
             for(int j = 0; j <= amount; j++) {
-                int notTake = 0 + dp[j];
+                int notTake = dp[j];
                 int take = (int)(1e9);
-                if(coins[i] <= j) take = 1 + temp[j - coins[i]];
-                temp[j] = Math.min(notTake, take);
+                if(coins[i] <= j) take = 1 + dp[j - coins[i]];
+                dp[j] = Math.min(notTake, take);
             }
-            dp = temp;
         }
 
         return dp[amount] == (int)(1e9) ? -1 : dp[amount];
     }
 }
-
 
 
 
