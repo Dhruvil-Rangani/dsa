8bf5f1a09230c6e548170c0333ef47bfb3344311
Dhruvil-Rangani <dhruvilrangani007@gmail.com>
2025-09-05 21:23:04 -0400

    Update coinChange2.java

--- dynamic programming/dp on subsequence/coinChange2.java
+++ dynamic programming/dp on subsequence/coinChange2.java
@@ -7,7 +7,7 @@ import java.util.*;
 // because we are making two calls at each step, so it is going to be more than this.
 // but this is not the optimal solution, we can do better than this using memoization or tabulation.
 // this is just to understand the problem and the recursive approach.
-class Solution {
+class Solution1 {
   private int f(int i, int target, int[] coins) {
     if(i == 0) {
       if(target % coins[0] == 0) return 1;
@@ -52,3 +52,31 @@ class Solution2 {
     return f(N - 1, amount, dp, coins);
   }
 }
+
+// tabulation approach
+// TC: O(N * amount)
+// SC: O(N * amount)
+// by using tabulation we can avoid the recursion stack space.
+class Solution3 {
+  private static final int mod = 1000000007;
+
+  public int count(int[] coins, int N, int amount) {
+    int[][] dp = new int[N][amount + 1];
+    for(int i = 0; i <= amount; i++) {
+      if(i % coins[0] == 0) dp[0][i] = 1;
+      else dp[0][i] = 0;
+    }
+
+    for(int i = 1; i < N; i++) {
+      for(int j = 0; j <= amount; j++) {
+        int notPick = dp[i - 1][j];
+        int pick = 0;
+        if(coins[i] <= j) pick = dp[i][j - coins[i]];
+        dp[i][j] = (pick + notPick) % mod;
+      }
+    }
+
+    return dp[N - 1][amount];
+  }
+}
+
