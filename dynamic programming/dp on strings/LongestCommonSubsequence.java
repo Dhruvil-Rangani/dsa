19a4aa8bd215c7489b05370ca86cc5e92ab79764
Dhruvil-Rangani <dhruvilrangani007@gmail.com>
2025-09-06 23:11:25 -0400

    Update LongestCommonSubsequence.java

--- dynamic programming/dp on strings/LongestCommonSubsequence.java
+++ dynamic programming/dp on strings/LongestCommonSubsequence.java
@@ -48,3 +48,25 @@ class Solution2 {
   }
 }
 
+// tabulation approach
+// TC: O(N * M)
+// SC: O(N * M)
+
+class Solution3 {
+  public int lcs(String str1, String str2) {
+    int n = str1.length();
+    int m = str2.length();
+    int[][] dp = new int[n + 1][m + 1];
+
+    for(int i = 1; i <= n; i++) {
+      for(int j = 1; j <= m; j++){
+        if(str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+        else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+      }
+    }
+
+    return dp[n][m];
+  }
+}
+
+
