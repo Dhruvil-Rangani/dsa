39a61702681608e47f0add75b98709c234d614bc
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-09 19:16:56 -0400

    Create distinctSubsequence.cpp

---
+++ dynamic programming/dp on strings/distinctSubsequence.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+// recursive approach
+// TC: O(2^n) SC: O(n+m) for recursion stack
+
+class Solution{
+private:
+  const int mod = 1e9 + 7;
+  int f(int i, int j, string s, string t) {
+    if(j < 0) return 1;
+    if(i < 0) return 0;
+
+    int result = 0;
+    if(s[i] == t[j]) {
+      int pick = f(i - 1, j - 1, s, t);
+      int notPick = f(i - 1, j, s, t);
+      result = (pick + notPick) % mod;
+    } else {
+      result = f(i - 1, j, s, t);
+    }
+
+    return result;
+  }
+public: 
+  int distinctSubsequences(string s, string t){
+     int n = s.size();
+     int m = t.size();
+
+     return f(n - 1, m - 1, s, t);
+  }
+};
