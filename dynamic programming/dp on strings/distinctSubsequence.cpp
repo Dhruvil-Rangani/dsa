cd9f5725d6770f5ccb5c371a02948a95e76e8123
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-09 22:22:13 -0400

    Update distinctSubsequence.cpp

--- dynamic programming/dp on strings/distinctSubsequence.cpp
+++ dynamic programming/dp on strings/distinctSubsequence.cpp
@@ -91,3 +91,27 @@ public:
         return dp[n][m];
     }
 };
+
+// space optimized approach
+// TC: O(n*m) SC: O(m)
+// 1D array can be used instead of 2D array because we are only using the previous row values
+class Solution4 {
+private:
+    const int mod = 1e9 + 7;
+
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<int> dp(m + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s[i - 1] == t[j - 1]) dp[j] = (dp[j - 1] + dp[j]) % mod;
+            }
+        }
+
+        return dp[m];
+    }
+};
