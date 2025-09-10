1811846a6b9575afab1726ca935bf71a4a68afd7
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-10 19:34:27 -0400

    Create editDistance.cpp

---
+++ dynamic programming/dp on strings/editDistance.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+// recursive approach
+// TC: O(3^(n+m)) SC: O(n+m) for recursion stack
+
+class Solution {
+  private:
+    int f(int i, int j, string start, string target) {
+      if(i < 0) return j + 1;
+      if(j < 0) return i + 1;
+      
+      if(start[i] == target[j]) return 0 + f(i - 1, j - 1, start, target);
+      else {
+
+        return 1 + min(f(i, j - 1, start, target), 
+                         min(f(i - 1, j, start, target),
+                              f(i -1, j - 1, start, target)));
+      }
+    }
+
+  public:
+    int editDistance(string start, string target) {
+      int n = start.size();
+      int m = target.size();
+
+      return f(n - 1, m - 1, start, target); 
+    }
+};
+
+// memoization approach
+// TC: O(n*m) SC: O(n*m) + O(n+m) for recursion stack
+
+
