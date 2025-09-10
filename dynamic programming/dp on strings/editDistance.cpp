77a9dad0eb966325d058e125741f543be0b862cc
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-10 19:38:17 -0400

    Update editDistance.cpp

--- dynamic programming/dp on strings/editDistance.cpp
+++ dynamic programming/dp on strings/editDistance.cpp
@@ -13,7 +13,6 @@ class Solution {
       
       if(start[i] == target[j]) return 0 + f(i - 1, j - 1, start, target);
       else {
-
         return 1 + min(f(i, j - 1, start, target), 
                          min(f(i - 1, j, start, target),
                               f(i -1, j - 1, start, target)));
@@ -31,5 +30,29 @@ class Solution {
 
 // memoization approach
 // TC: O(n*m) SC: O(n*m) + O(n+m) for recursion stack
+class Solution {
+  private:
+    int f(int i, int j, string start, string target, vector<vector<int>>& dp) {
+      if(i < 0) return j + 1;
+      if(j < 0) return i + 1;
+      if(dp[i][j] != -1) return dp[i][j];
+      if(start[i] == target[j]) return dp[i][j] = 0 + f(i - 1, j - 1, start, target, dp);
+      else {
+        // Insert
+        return dp[i][j] = 1 + min(f(i, j - 1, start, target, dp), 
+                         min(f(i - 1, j, start, target, dp),
+                              f(i -1, j - 1, start, target, dp)));
+      }
+    }
+
+  public:
+    int editDistance(string start, string target) {
+      int n = start.size();
+      int m = target.size();
+      vector<vector<int>> dp(n, vector<int>(m, -1));
+      return f(n - 1, m - 1, start, target, dp); 
+    }
+};
+
 
 
