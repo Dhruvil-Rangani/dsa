928bb109418cb421b631f98d421f48c95c8d9084
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-10 20:08:11 -0400

    Update editDistance.cpp

--- dynamic programming/dp on strings/editDistance.cpp
+++ dynamic programming/dp on strings/editDistance.cpp
@@ -83,6 +83,38 @@ public:
     }
 };
 
+// space optimization
+// TC: O(n*m) SC: O(m)
+class Solution {
+public:
+    int minDistance(string start, string target) {
+        int n = start.size();
+        int m = target.size();
+        vector<int> dp(m + 1, 0);
+        vector<int> temp(m + 1, 0);
+        for (int i = 0; i <= m; i++)
+            dp[i] = i;
+
+        for (int i = 1; i <= n; i++) {
+            temp[0] = i;
+            for (int j = 1; j <= m; j++) {
+                if (start[i - 1] == target[j - 1])
+                    temp[j] = dp[j - 1];
+                else {
+                    temp[j] = 1 + min(temp[j - 1], min(dp[j], dp[j - 1]));
+                }
+            }
+            dp = temp;
+        }
+
+        return dp[m];
+    }
+};
+
+// space optimization 2 with just one array
+// TC: O(n*m) SC: O(m)
+
+
 
 
 
