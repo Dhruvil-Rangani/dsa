a77f7fbb3e5b39d83b4b067e37ab58d2599758c2
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-11 16:29:05 -0400

    Update minWindowSubsequence.cpp

--- dynamic programming/dp on subsequence/minWindowSubsequence.cpp
+++ dynamic programming/dp on subsequence/minWindowSubsequence.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <climits>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 // recursive approach
 // TC: O(2^(n+m)) SC: O(n+m) for recursion stack
@@ -92,5 +93,41 @@ public:
         return length > n ? "" : s1.substr(end - length, length);
     }
 };
+
+// optimal approach - using greedy + binary search
+// TC: worst case O(n*m) SC: O(n) for map
+class Solution {
+public:
+    string minWindow(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        
+        string answer;
+
+        unordered_map<char, vector<int>> indices;
+
+        for(int i = 0; i < n; i++) indices[s1[i]].push_back(i);
+
+        vector<int> ind(m);
+
+        for(int start = 0; start < n; start++) {
+            int prev = start - 1;
+            for(int j = 0; j < m; j++) {
+                if(!indices.count(s2[j])) return "";
+                const vector<int>& curIndices = indices[s2[j]];
+                while(ind[j] < curIndices.size() && curIndices[ind[j]] <= prev) ind[j]++;
+                if(ind[j] == curIndices.size()) return answer;
+                prev = curIndices[ind[j]];
+            }
+
+            if (answer == "" || prev - start + 1 < answer.size()) {
+                answer = s1.substr(start, prev - start + 1);
+            }
+        }
+
+        return answer;
+    }
+};
+
 
 
