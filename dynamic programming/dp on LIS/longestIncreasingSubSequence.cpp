daaccad460720911dc742505d11e7a52e425198c
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-09-15 19:05:12 -0400

    Update longestIncreasingSubSequence.cpp

--- dynamic programming/dp on LIS/longestIncreasingSubSequence.cpp
+++ dynamic programming/dp on LIS/longestIncreasingSubSequence.cpp
@@ -29,3 +29,28 @@ public:
         return f(0, -1, nums, dp);
     }    
 };
+
+// binary search approach
+// TC: O(nlogn)
+// SC: O(n)
+
+class Solution {
+public:
+    int LIS(vector<int>& nums) {
+      int n = nums.size();
+
+      vector<int> temp;
+      temp.push_back(nums[0]);
+
+      for(int i = 1; i < n; i++) {
+        if(nums[i] > temp.back()) {
+            temp.push_back(nums[i]);
+        } else {
+            int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+            temp[ind] = nums[i];
+        }
+      }
+
+      return temp.size();
+    }    
+};
